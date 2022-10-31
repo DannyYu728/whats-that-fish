91abb703e20e8da4afbbcfdb40d0a155a55d970c
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-10-31 10:14:22 -0400

    update script rmove close

--- seed/updateSeed.js
+++ seed/updateSeed.js
@@ -14,7 +14,6 @@ let updateData = async () => {
     );
   });
 
-  db.close()
 };
 
 updateData()
--- seed/updateSeed2.js
+++ seed/updateSeed2.js
@@ -15,7 +15,6 @@ let updateData = async () => {
     );
   });
 
-  db.close()
 };
 
 updateData()
